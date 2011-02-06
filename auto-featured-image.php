75656369a7128f2a7e7bbc783b7cfd33dc84e3ac
Ka Yue <kayuey@gmail.com>
2011-02-06 13:52:03 +0800

    Udated admin panel setting

--- auto-featured-image.php
+++ auto-featured-image.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Auto Featured Image
  * Description: Automatically generate featured image for new/old posts if Post Thumbnail is not set manually. In addition to post attachment, it also support external image, Youtube, Vimeo, DailyMotion. Originally designed by Aditya Mooley <adityamooley@sanisoft.com>.
- * Version: 1.1
+ * Version: 1.2
  * Author: Ka Yue Yeung <kayuey@gmail.com>
  * Author URI: http://ka-yue.com
  */
@@ -11,9 +11,8 @@ add_action( 'save_post', array("AutoFeautredImagePlugin", "generate_thumbmail") 
 add_action( 'transition_post_status', array("AutoFeautredImagePlugin", "check_transition") ); // Plugin should work for scheduled posts as well
 add_action( 'admin_notices', array("AutoFeautredImagePlugin", "check_permission") );
 
-add_action( 'admin_menu', 'afi_add_admin_menu'); // Add batch process capability
-add_action( 'admin_enqueue_scripts', 'afi_admin_enqueues' ); // Plugin hook for adding CSS and JS files required for this plugin
-add_action( 'wp_ajax_generatepostthumbnail', 'afi_ajax_process_post' ); // Hook to implement AJAX request
+add_action( 'admin_menu', array("AutoFeautredImagePlugin", "add_admin_menu") ); // Add batch process capability
+add_action( 'wp_ajax_generatepostthumbnail', array("AutoFeautredImagePlugin", "ajax_process_post") ); // Hook to implement AJAX request
 
 class AutoFeautredImagePlugin {
     
@@ -25,30 +24,32 @@ class AutoFeautredImagePlugin {
      * @param $post
      * @return void
      */
-    public function check_transition( $new_status='', $old_status='', $post='' ) 
+    static function check_transition( $new_status='', $old_status='', $post='' ) 
     {
         if ('publish' == $new_status && 'future' == $old_status) {
-            afi_publish_post($post->ID);
+            self::generate_thumbmail($post->ID);
         }
     }
-    
     
     /**
      * Function to save first image in post as post thumbmail.
      */
-    public function generate_thumbmail( $post_id )
+    static function generate_thumbmail( $post_id )
     {
         global $wpdb;
         
         $post = get_post($dummy_wp = $post_id);
         
+        // reset post parent id
+        $post_parent_id = $post->post_parent === 0 ? $post->ID : $post->post_parent;
+        
         // check whether Post Thumbnail is already set for this post.
-        if ( has_post_thumbnail($post->post_parent) ) return;
+        if ( has_post_thumbnail($post_parent_id) ) return "has thumbnail";
         
         // case 1: there is an image attachment we can use
         // found all images attachments from the post
         $attachments = array_values(get_children(array(
-            'post_parent' => $post->post_parent, 
+            'post_parent' => $post_parent_id, 
             'post_status' => 'inherit', 
             'post_type' => 'attachment', 
             'post_mime_type' => 'image', 
@@ -58,7 +59,7 @@ class AutoFeautredImagePlugin {
         
         // if attachment found, set the first attachment as thumbnail
         if( sizeof($attachments) > 0 ) {
-            update_post_meta( $post->post_parent, '_thumbnail_id', $attachments[0]->ID );
+            update_post_meta( $post_parent_id, '_thumbnail_id', $attachments[0]->ID );
             return;
         }
         
@@ -75,10 +76,11 @@ class AutoFeautredImagePlugin {
         
         // update post thumbnail meta if thumbnail found
         if(is_int($post_thumbnail_id)) {
-            update_post_meta( $post->post_parent, '_thumbnail_id', $post_thumbnail_id );
+            update_post_meta( $post_parent_id, '_thumbnail_id', $post_thumbnail_id );
         }
+        
+        return;
     }
-    
     
     /**
      * @return Integer if attachment id if attachment is used. 
@@ -233,48 +235,49 @@ class AutoFeautredImagePlugin {
             echo '</p></div>';
         }
     }
-}
-
-// Register the management page
-function afi_add_admin_menu() {
-    // add_options_page('Auto Featured Image', 'Auto Post Thumbnail', 'manage_options', 'generate-post-thumbnails', 'afi_interface');
-}
-
-/**
- * Admin user interface plus post thumbnail generator
- * 
- * Most of the code in this function is copied from - 
- * Regenerate Thumbnails plugin (http://www.viper007bond.com/wordpress-plugins/regenerate-thumbnails/)
- * 
- * @return void
- */
-function afi_interface() {
-    global $wpdb;
-?>
-<div id="message" class="updated fade" style="display:none"></div>
-
-<div class="wrap genpostthumbs">
-    <h2>Generate Post Thumbnails</h2>
     
-<?php 
-    // If the button was clicked
-        if ( !empty($_POST['generate-post-thumbnails']) ) {
+    static function add_admin_menu() // Register the management page
+    {
+        add_options_page('Auto Featured Image', 'Auto Featured Image', 'manage_options', 'generate-post-thumbnails', array( "AutoFeautredImagePlugin", "construct_admin_interface"));
+    }
+    
+    
+    /**
+     * Admin user interface plus post thumbnail generator
+     * 
+     * Most of the code in this function is copied from - 
+     * Regenerate Thumbnails plugin (http://www.viper007bond.com/wordpress-plugins/regenerate-thumbnails/)
+     * 
+     * @return void
+     */
+    static function construct_admin_interface() {
+        global $wpdb;
+        ?>
+        
+        <div class="wrap genpostthumbs">
+            <h2>Auto Featured Image</h2>
+            
+        <?php 
+        // If the button was clicked
+        if ( !empty($_POST['generate-post-thumbnails']) ) :
+            
             // Capability check
-            if ( !current_user_can('manage_options') )
-                wp_die('Cheatin&#8217; uh?');
-
+            if ( !current_user_can('manage_options') ) wp_die('Cheatin&#8217; uh?');
+            
             // Form nonce check
             check_admin_referer( 'generate-post-thumbnails' );
             
             // Get id's of all the published posts for which post thumbnails does not exist.
-            $query = "SELECT * FROM {$wpdb->posts} p where p.post_status = 'publish' AND post_type = 'post' AND p.ID NOT IN (
+            $query = "SELECT ID FROM {$wpdb->posts} p WHERE p.post_status = 'publish' AND post_type = 'post' AND p.ID NOT IN (
                         SELECT DISTINCT post_id FROM {$wpdb->postmeta} WHERE meta_key IN ('_thumbnail_id', 'skip_post_thumb')
-                      )";
+                      ) ORDER BY ID DESC";
+            
             $posts = $wpdb->get_results($query);
             
-            if (empty($posts)) {
+            if ( empty($posts) ) {
                 echo '<p>Currently there are no published posts available to generate thumbnails.</p>';
             } else {
+                
                 echo '<p>We are generating post thumbnails. Please be patient!</p>';
                 
                 // Generate the list of IDs
@@ -282,107 +285,86 @@ function afi_interface() {
                 foreach ( $posts as $post )
                     $ids[] = $post->ID;
                 $ids = implode( ',', $ids );
-
+    
                 $count = count( $posts );
-?>
-    <noscript><p><em>You must enable Javascript in order to proceed!</em></p></noscript>
-
-    <div id="genpostthumbsbar" style="position:relative;height:25px;">
-        <div id="genpostthumbsbar-percent" style="position:absolute;left:50%;top:50%;width:50px;margin-left:-25px;height:25px;margin-top:-9px;font-weight:bold;text-align:center;"></div>
-    </div>
-
-    <script type="text/javascript">
-    // <![CDATA[
-        jQuery(document).ready(function($){
-            var i;
-            var rt_images = [<?php echo $ids; ?>];
-            var rt_total = rt_images.length;
-            var rt_count = 1;
-            var rt_percent = 0;
-
-            $("#genpostthumbsbar").progressbar();
-            $("#genpostthumbsbar-percent").html( "0%" );
-
-            function genPostThumb( id ) {
-                $.post( "admin-ajax.php", { action: "generatepostthumbnail", id: id }, function() {
-                    rt_percent = ( rt_count / rt_total ) * 100;
-                    $("#genpostthumbsbar").progressbar( "value", rt_percent );
-                    $("#genpostthumbsbar-percent").html( Math.round(rt_percent) + "%" );
-                    rt_count = rt_count + 1;
-
-                    if ( rt_images.length ) {
-                        genPostThumb( rt_images.shift() );
-                    } else {
-                        $("#message").html("<p><strong><?php echo js_escape( sprintf('All done! Processed %d posts.', $count ) ); ?></strong></p>");
-                        $("#message").show();
-                    }
-
-                });
+            ?>
+            
+            <div id="message">Processing...</div>
+            
+            <noscript><p><em>You must enable Javascript in order to proceed!</em></p></noscript>
+            
+            <script type="text/javascript">
+            // <![CDATA[
+            jQuery(document).ready(function($){
+                var i;
+                var postIds = [<?php echo $ids; ?>];
+                var totalPosts = postIds.length;
+                var processedPostCount = 1;
+                var $message = $("#message");
+                
+                function genPostThumb( id ) {
+                    $.post( "admin-ajax.php", { action: "generatepostthumbnail", id: id }, function() {
+                        var percentDone = ( processedPostCount / totalPosts ) * 100;
+                        
+                        $message.text( "Processing post #" + id + ". " + Math.round(percentDone) + "% done." );
+                        
+                        processedPostCount++;
+                        
+                        if ( postIds.length ) {
+                            genPostThumb( postIds.shift() );
+                        } else {
+                            $message
+                                .addClass("updated")
+                                .html("<p><strong>All done! Processed "+totalPosts+" posts.</strong></p>");
+                        }
+                        
+                    });
+                }
+                
+                // start ajax
+                genPostThumb( postIds.shift() );
+            });
+            // ]]>
+            </script>
+            <?php
             }
-
-            genPostThumb( rt_images.shift() );
-        });
-    // ]]>
-    </script>
-<?php
-            }
-        } else {
-?>
+        else : // button is not clicked
+        ?>
+            <p>Use this tool to auto generate featured image for your published posts.</p>
+            <p>If the script stops executing for any reason, just <strong>reload</strong> the page and it will continue from where it stopped.</p>
+            
+            <form method="post" action="">
+                <?php wp_nonce_field('generate-post-thumbnails') ?>
+                <p><input type="submit" class="button hide-if-no-js" name="generate-post-thumbnails" id="generate-post-thumbnails" value="Generate Thumbnails" /></p>
+                <noscript><p><em>You must enable Javascript in order to proceed!</em></p></noscript>
+            </form>
+            
+        <? endif; ?>
+        </div>
+    <?php
+    } //End afi_interface()
     
-    <p>Use this tool to generate Post Thumbnail (Featured Thumbnail) for your Published posts.</p>
-    <p>If the script stops executing for any reason, just <strong>Reload</strong> the page and it will continue from where it stopped.</p>
-
-    <form method="post" action="">
-<?php wp_nonce_field('generate-post-thumbnails') ?>
-
-
-    <p><input type="submit" class="button hide-if-no-js" name="generate-post-thumbnails" id="generate-post-thumbnails" value="Generate Thumbnails" /></p>
-
-    <noscript><p><em>You must enable Javascript in order to proceed!</em></p></noscript>
-
-    </form>
-    <p>Note: Thumbnails won't be generated for posts that already have post thumbnail or <strong><em>skip_post_thumb</em></strong> custom field set.</p>
-<?php } ?>
-</div>
-<?php
-} //End afi_interface()
-
-/**
- * Add our JS and CSS files
- * 
- * @param $hook_suffix
- * @return void
- */
-function afi_admin_enqueues($hook_suffix) {
-    if ( 'settings_page_generate-post-thumbnails' != $hook_suffix ) {
-        return;
-    }
-
-    wp_enqueue_script( 'jquery-ui-progressbar', plugins_url( 'jquery-ui/ui.progressbar.js', __FILE__ ), array('jquery-ui-core'), '1.7.2' );
-    wp_enqueue_style( 'jquery-ui-genpostthumbs', plugins_url( 'jquery-ui/redmond/jquery-ui-1.7.2.custom.css', __FILE__ ), array(), '1.7.2' );
-} //End afi_admin_enqueues
-
-/**
- * Process single post to generate the post thumbnail
- * 
- * @return void
- */
-function afi_ajax_process_post() {
-    if ( !current_user_can( 'manage_options' ) ) {
-        die('-1');
-    }    
-
-    $id = (int) $_POST['id'];
+    /**
+     * Process single post to generate the post thumbnail
+     * 
+     * @return void
+     */
+    static function ajax_process_post() {
+        if ( !current_user_can( 'manage_options' ) ) {
+            die('-1');
+        }    
     
-    if ( empty($id) ) {
-        die('-1');
-    }
-    
-    set_time_limit( 60 );
-    
-    // Pass on the id to our 'publish' callback function.
-    afi_publish_post($id);
-    
-    die(-1);
-} //End afi_ajax_process_post()
-
+        $id = (int) $_POST['id'];
+        
+        if ( empty($id) ) {
+            die('-1');
+        }
+        
+        set_time_limit( 60 );
+        
+        // Pass on the id to our 'publish' callback function.
+        echo self::generate_thumbmail($id);
+        
+        die(-1);
+    } //End ajax_process_post()
+}
